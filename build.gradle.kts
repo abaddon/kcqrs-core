d4403461b04ae716bd89eab24f42941380173a82
Stefano <34969+abaddon@users.noreply.github.com>
2022-02-04 11:58:56 +0000

    remove other modules added deploy on maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,72 +1,166 @@
-//plugins {
-//    kotlin("jvm") version "1.5.31"
-//    kotlin("plugin.serialization") version "1.5.31" apply true
-//    jacoco
-//}
-//
-//group = "io.github.abaddon.kcqrs"
-//version = "0.0.1-SNAPSHOT"
-//
-////Versions
-//val slf4jVersion = "1.7.25"
-//val kotlinVersion = "1.5.31"
-//val kotlinCoroutineVersion = "1.5.1"
-//val kotlinxSerializationVersion = "1.3.0"
-//val jacksonModuleKotlinVersion = "2.13.0"
-//val junitJupiterVersion = "5.7.0"
-//
-//val eventStoreDBVersion = "1.0.0"
-//
-//repositories {
-//    mavenCentral()
-//}
-//
-//dependencies {
-//    //Core
-//    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-//    implementation("org.slf4j:slf4j-log4j12:$slf4jVersion")
-//    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutineVersion")
-//    //implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
-//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonModuleKotlinVersion")
-//
-//    //EventStoreDB
-//    implementation("com.eventstore:db-client-java:$eventStoreDBVersion")
-//
-//
-//    testImplementation(kotlin("test"))
-//    testImplementation("org.junit.jupiter:junit-jupiter:${junitJupiterVersion}") // JVM dependency
-//    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinCoroutineVersion")
-//    testImplementation("org.testcontainers:testcontainers:1.16.2")
-//    testImplementation("org.testcontainers:junit-jupiter:1.16.2")
-//
-//
-//
-//}
-//
-//jacoco {
-//    toolVersion = "0.8.7"
-//    //reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
-//}
-//
-//tasks.withType<Test> {
-//    useJUnitPlatform()
-//    testLogging {
-//        events("passed", "skipped", "failed")
-//    }
-//}
-//
-//tasks.jacocoTestReport {
-//    reports {
-//        xml.required.set(true)
-//        csv.required.set(false)
-//        html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco"))
-//    }
-//}
-//
-//
-//tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
-//    kotlinOptions.jvmTarget = "11"
-//}
-//
-//
+
+group = "io.github.abaddon.kcqrs"
+version = "0.0.1-SNAPSHOT"
+
+object Meta {
+    const val desc = "Kotlin CQRS Core library"
+    const val license = "Apache-2.0"
+    const val githubRepo = "abaddon/kcqrs-core"
+    const val release = "https://s01.oss.sonatype.org/service/local/"
+    const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
+
+object Versions {
+    const val slf4jVersion = "1.7.25"
+    const val kotlinVersion = "1.6.0"
+    const val kotlinCoroutineVersion = "1.6.0"
+    const val jacksonModuleKotlinVersion = "2.13.0"
+    const val junitJupiterVersion = "5.7.0"
+    const val jacocoToolVersion = "0.8.7"
+    const val jvmTarget = "11"
+}
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    jacoco
+    `maven-publish`
+    signing
+}
+
+//Versions
+
+
+publishing {
+    publications {
+        create<MavenPublication>("kcqrs-core") {
+            from(components["kotlin"])
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //Core
+    implementation("org.slf4j:slf4j-api:${Versions.slf4jVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlinVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlinCoroutineVersion}")
+
+    //Test
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:${Versions.junitJupiterVersion}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.kotlinCoroutineVersion}")
+}
+
+jacoco {
+    toolVersion = Versions.jacocoToolVersion
+    //reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco"))
+    }
+}
+
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
+    kotlinOptions.jvmTarget = Versions.jvmTarget
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+signing {
+    val signingKey = providers
+        .environmentVariable("GPG_SIGNING_KEY")
+        .forUseAtConfigurationTime()
+    val signingPassphrase = providers
+        .environmentVariable("GPG_SIGNING_PASSPHRASE")
+        .forUseAtConfigurationTime()
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+        val extension = extensions
+            .getByName("publishing") as PublishingExtension
+        sign(extension.publications)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name.set(project.name)
+                description.set(Meta.desc)
+                url.set("https://github.com/${Meta.githubRepo}")
+                licenses {
+                    license {
+                        name.set(Meta.license)
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hpehl")
+                        name.set("Harald Pehl")
+                        organization.set("Red Hat")
+                        organizationUrl.set("https://developers.redhat.com/")
+                    }
+                }
+                scm {
+                    url.set(
+                        "https://github.com/${Meta.githubRepo}.git"
+                    )
+                    connection.set(
+                        "scm:git:git://github.com/${Meta.githubRepo}.git"
+                    )
+                    developerConnection.set(
+                        "scm:git:git://github.com/${Meta.githubRepo}.git"
+                    )
+                }
+                issueManagement {
+                    url.set("https://github.com/${Meta.githubRepo}/issues")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri(Meta.release))
+            snapshotRepositoryUrl.set(uri(Meta.snapshot))
+            val ossrhUsername = providers
+                .environmentVariable("OSSRH_USERNAME")
+                .forUseAtConfigurationTime()
+            val ossrhPassword = providers
+                .environmentVariable("OSSRH_PASSWORD")
+                .forUseAtConfigurationTime()
+            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
+                username.set(ossrhUsername.get())
+                password.set(ossrhPassword.get())
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,1 @@
-rootProject.name = "kotlin-cqrs"
-include("kcqrs-test")
-include("kcqrs-core")
-include("kustomCompare")
-include("kcqrs-EventStoreDB")
-include("kcqrs-example")
-
+rootProject.name = "kcqrs-core"
