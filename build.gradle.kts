fce2dbdfff0536c00f5994ad7afb97985eed56ae
Stefano Longhi <abaddon83@gmail.com>
2022-03-18 00:37:04 +0000

    Update release script

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,7 +104,7 @@ signing {
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("kcqrs-core") {
             groupId = project.group.toString()
             artifactId = project.name
             version = project.version.toString()
