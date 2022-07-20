ff40d38b7fe0d7f44599d9b2bcb9ba4028d8a094
Stefano <34969+abaddon@users.noreply.github.com>
2022-07-20 21:15:17 +0100

    Improve SimpleAggregateCommandHandler - add aggregate version validation - add onSuccess and onFailure methods

--- src/main/kotlin/io/github/abaddon/kcqrs/core/domain/SimpleAggregateCommandHandler.kt
+++ src/main/kotlin/io/github/abaddon/kcqrs/core/domain/SimpleAggregateCommandHandler.kt
@@ -2,10 +2,11 @@ package io.github.abaddon.kcqrs.core.domain
 
 import io.github.abaddon.kcqrs.core.IAggregate
 import io.github.abaddon.kcqrs.core.domain.messages.commands.ICommand
+import io.github.abaddon.kcqrs.core.domain.messages.events.IDomainEvent
 import io.github.abaddon.kcqrs.core.persistence.IAggregateRepository
 import java.util.*
 
-class SimpleAggregateCommandHandler<TAggregate : IAggregate>(
+open class SimpleAggregateCommandHandler<TAggregate : IAggregate>(
     override val repository: IAggregateRepository<TAggregate>,
 ) : IAggregateCommandHandler<TAggregate> {
     override suspend fun handle(
@@ -14,12 +15,25 @@ class SimpleAggregateCommandHandler<TAggregate : IAggregate>(
     ): Result<Exception, TAggregate> =
         when (val actualAggregateResult = repository.getById(command.aggregateID)) {
             is Result.Valid -> {
-                val newAggregate = command.execute(actualAggregateResult.value)
-                repository.save(newAggregate, UUID.randomUUID(), updateHeaders)
+                try {
+                    val newAggregate = command.execute(actualAggregateResult.value)
+                    val saveResult=repository.save(newAggregate, UUID.randomUUID(), updateHeaders)
+                    when (saveResult) {
+                        is Result.Valid -> onSuccess(newAggregate)
+                        is Result.Invalid -> onFailure(saveResult.err)
+                    }
+                    saveResult
+                } catch (ex: Exception){
+                    Result.Invalid(ex)
+                }
             }
             is Result.Invalid -> actualAggregateResult
         }
 
+    open fun onSuccess(updatedAggregate:  TAggregate){}
+    open fun onFailure(  err:Exception){}
+
     override suspend fun handle(command: ICommand<TAggregate>): Result<Exception, TAggregate> =
         handle(command) { mapOf<String, String>() }
+
 }
